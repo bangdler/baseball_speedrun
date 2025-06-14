3ba3bfcaca154006d7cc93013529403230c2efaa
bangdler <zbthz90@gmail.com>
2025-06-14 21:46:19 +0900

    chore: tailwind 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LayoutRecoil from "./layout.recoil";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import QueryProvider from "../provider/QueryProvider";
+import "../globals.css";
 
 export const metadata = {
   title: "myfair front pre-course",
