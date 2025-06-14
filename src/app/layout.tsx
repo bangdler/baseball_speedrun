20eaa941b2f55b011e1ce4ecd6d3d7ca27dee9f8
bangdler <zbthz90@gmail.com>
2025-06-14 20:33:37 +0900

    chore: tailwind, tanstack query 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import LayoutRecoil from "./layout.recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import QueryProvider from "../provider/QueryProvider";
 
 export const metadata = {
   title: "myfair front pre-course",
@@ -14,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <LayoutRecoil>{children}</LayoutRecoil>
+        <QueryProvider>
+          <LayoutRecoil>{children}</LayoutRecoil>
+        </QueryProvider>
       </body>
     </html>
   );
